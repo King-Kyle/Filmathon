220ba3b92d3c4b4febd606b1ba8dc81b8fa4c746
Kyle <kylek918@icloud.com>
2019-01-25 16:29:18 +0200

    Updates

---
+++ precache-manifest.d3535bdf57fc7f859a16eaf3a6604ff6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "015abc8ec18339e85bbdada2360a5539",
+    "url": "/myapp/static/media/placeholder-img.015abc8e.jpg"
+  },
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "d850ebdf31abe7fbd0cb",
+    "url": "/myapp/static/js/main.d850ebdf.chunk.js"
+  },
+  {
+    "revision": "09d48fecbdafe6faad95",
+    "url": "/myapp/static/js/1.09d48fec.chunk.js"
+  },
+  {
+    "revision": "d850ebdf31abe7fbd0cb",
+    "url": "/myapp/static/css/main.b768d3a3.chunk.css"
+  },
+  {
+    "revision": "09d48fecbdafe6faad95",
+    "url": "/myapp/static/css/1.f58c3f44.chunk.css"
+  },
+  {
+    "revision": "a4259c163fe020b3f90c15cfc77e6052",
+    "url": "/myapp/index.html"
+  }
+];
